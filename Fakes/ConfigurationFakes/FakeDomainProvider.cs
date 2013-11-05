77caa8f655dc5948406b1b439e2e7463339cf5e0
Thomas Stern <git@istern.dk>
2013-11-05 20:17:27 +0100

    added missing files

---
+++ Fakes/ConfigurationFakes/FakeDomainProvider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Sitecore.Security.Domains;
+using Sitecore.SecurityModel;
+
+namespace Sitecore.Fakes.ConfigurationFakes
+{
+    public class FakeDomainProvider : DomainProvider
+    {
+        public override void AddDomain(string domainName, bool locallyManaged)
+        {
+            
+        }
+
+        public override Domain GetDomain(string name)
+        {
+           return new Domain(name);
+        }
+
+        public override IEnumerable<Domain> GetDomains()
+        {
+            return new List<Domain>();
+        }
+
+        public override void RemoveDomain(string domainName)
+        {
+            
+        }
+    }
+}
